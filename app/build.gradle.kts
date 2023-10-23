d3982ac8c341be83d5d8ced73086e33142d9fe24
Shiina Sugioka <shiina12@uw.edu>
2023-10-23 13:52:23 -0700

    builds successfully

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "edu.uw.ischool.shiina12.biggobutton"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "edu.uw.ischool.shiina12.biggobutton"
