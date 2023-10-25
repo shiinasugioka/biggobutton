41d87cf6279de1cf081cd7cbab148b12881f6446
Shiina Sugioka <shiina12@uw.edu>
2023-10-25 16:26:52 -0700

    finished button

--- app/src/main/java/edu/uw/ischool/shiina12/biggobutton/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/shiina12/biggobutton/MainActivity.kt
@@ -2,10 +2,25 @@ package edu.uw.ischool.shiina12.biggobutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myButton = findViewById<Button>(R.id.greenButton)
+        var counter = 0
+
+        myButton.setOnClickListener {
+            counter++
+            Log.i("BUTTON", "pressed")
+            if (counter == 1) {
+                myButton.text = "You have pushed me 1 time!"
+            } else {
+                myButton.text = "You have pushed me $counter times!"
+            }
+        }
     }
 }
